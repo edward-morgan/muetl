13cefb26fc97041af780ca89ebd47d88f7a29acf
Edward Morgan <emorgan@teamraft.com>
2025-11-11 17:23:12 -0500

    add todos

--- src/runtime/event.rs
+++ src/runtime/event.rs
@@ -7,6 +7,12 @@ use crate::messages::event::Event;
 /// functions should need to care about.
 pub struct InternalEvent {
     /// The sender's ID as set at runtime when instantiated by the system.
+    /// A sender_id is composed of the unique ID of the sending actor + an ID representing the connection it's being sent on.
     pub sender_id: u64,
     pub event: Event,
 }
+
+// TODOs:
+// 1. InternalEvent needs to be published by anything that has outputs, NOT Event. This will make the Sink work as expected.
+// 2. Runtime needs to publish sender IDs
+// 3. Test with a mock Sink
